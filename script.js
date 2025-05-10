d5b2126c679e650aee5ab40dc0bb9503b14dae81
kalyanam17abhinav <kalyanamabhinav83916@gmail.com>
2025-05-10 22:56:06 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// alert("kalyanam abhinav");
+
+const menuBtn = document.querySelector('.menu-button');
+const menuBtn2 = document.querySelector('.menu-button2');
+const menuOpen = document.querySelector('.menu-open');
+
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.add("hidden");
+    menuBtn2.classList.remove('hidden');
+    menuOpen.classList.remove('hidden');
+});
+
+menuBtn2.addEventListener('click', () => {
+    menuBtn2.classList.add('hidden');
+    menuOpen.classList.add('hidden');
+    menuBtn.classList.remove('hidden');
+});
